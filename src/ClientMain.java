147c751430f3ccbc622c9ffffda7e97164e298c2
ming-do-o <zzzxzxc369@naver.com>
2021-04-09 10:03:29 +0900

    하하

--- src/ClientMain.java
+++ src/ClientMain.java
@@ -3,6 +3,6 @@ public class ClientMain {
 	public static void main(String[] args) {
 		ClientLoginPage clp = new ClientLoginPage();
 		System.out.println("우리도 깃허브 쓴다");
-		System.out.println("짜증 왜 안되노");
+		System.out.println("fuck you");
 	}
 }
