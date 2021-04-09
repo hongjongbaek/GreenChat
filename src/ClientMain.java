53559d312f14a9539f95f7f64185e51352b958f4
user <user@DESKTOP-E94K01S>
2021-04-09 10:14:47 +0900

    서버 클라이언트 연결

--- src/ClientMain.java
+++ src/ClientMain.java
@@ -1,8 +1,59 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class ClientMain {
-	
+	private static final int PORT = 1239;
+
 	public static void main(String[] args) {
 		ClientLoginPage clp = new ClientLoginPage();
-		System.out.println("Ïö∞Î¶¨ÎèÑ ÍπÉÌóàÎ∏å Ïì¥Îã§");
-		System.out.println("fuck you");
+		Socket socket = null;
+		try {
+			socket = new Socket("localhost", PORT);
+			DataInputStream dis = new DataInputStream(socket.getInputStream());
+			DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+			Scanner scanner = new Scanner(System.in);
+			
+			System.out.println("ªÁøÎ¿⁄ µÈæÓ∞°øÍ!");
+			String chk = scanner.nextLine();
+			
+			dos.writeUTF(chk);
+			dos.flush();
+			
+			Thread readServerThread = new Thread(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						String fromServer = null;
+						while((fromServer = dis.readUTF()) != null ) {
+							System.out.println(fromServer);
+						}
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			});
+//			readServerThread.setDaemon(true);
+			readServerThread.start();
+			while(true) {
+				String message = scanner.nextLine();
+				dos.writeUTF(message);
+				dos.flush();
+			} 
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				socket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
 	}
 }
--- src/Server.java
+++ src/Server.java
@@ -1,20 +1,38 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Server {
-	private static final int PORT = 4885;
+	private static final int PORT = 1239;
+	private static List<PrintWriter> list = new ArrayList<>();
 	
+	public synchronized static void addSocket(PrintWriter socket) {
+		list.add(socket);
+	}
+	public synchronized static void removeSocket(PrintWriter socket) {
+		list.remove(socket);
+	}
+	public synchronized static void output(String message) {
+		for(PrintWriter writer : list) {
+			writer.println(message);
+			writer.flush();
+		}
+	}
 	public static void main(String[] args) {
 		try (ServerSocket server = new ServerSocket(PORT)) {
-			System.out.println("클라이언트를 기다리는 중...");
-			try (Socket socket = server.accept();) {
-				System.out.println("클라이언트 접속 중...");
-				DataInputStream input = new DataInputStream(socket.getInputStream());
-				DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+			while (true) {
+				System.out.println("클라이언트를 기다리는 중...");
+				Socket socket = server.accept();
 				
+				System.out.println("클라이언트 접속 중...");
+//				Socket client = server.accept();
+				ChatThread chatThread = new ChatThread(socket);
+				chatThread.start();
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -22,4 +40,40 @@ public class Server {
 			System.out.println("클라이언트 접속 종료...");
 		}
 	}
+}
+
+class ChatThread extends Thread {
+	private Socket socket;
+	private BufferedReader reader;
+	private PrintWriter writer;
+	
+	public ChatThread(Socket socket) {
+		super();
+		this.socket = socket;
+		try {
+			reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			writer = new PrintWriter(socket.getOutputStream());
+						Server.addSocket(writer);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	public void run() {
+		try {
+			String message = null;
+			while ((message = reader.readLine()) != null) {
+				Server.output(message);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				Server.removeSocket(writer);
+				socket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
 }
