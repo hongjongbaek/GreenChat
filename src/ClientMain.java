8c34302e613e2888f44242766ba3fcf0db407caa
0 <0@192.168.219.105>
2021-04-12 16:03:20 +0900

    회원가입 완료

--- src/ClientMain.java
+++ src/ClientMain.java
@@ -1,5 +1,3 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -13,36 +11,11 @@ public class ClientMain {
 		Socket socket = null;
 		try {
 			socket = new Socket("localhost", PORT);
-			DataInputStream dis = new DataInputStream(socket.getInputStream());
-			DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
 			Scanner scanner = new Scanner(System.in);
 			
 			System.out.println("사용자 들어가욧!");
 			String chk = scanner.nextLine();
 			
-			dos.writeUTF(chk);
-			dos.flush();
-			
-			Thread readServerThread = new Thread(new Runnable() {
-				@Override
-				public void run() {
-					try {
-						String fromServer = null;
-						while((fromServer = dis.readUTF()) != null ) {
-							System.out.println(fromServer);
-						}
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
-			});
-//			readServerThread.setDaemon(true);
-			readServerThread.start();
-			while(true) {
-				String message = scanner.nextLine();
-				dos.writeUTF(message);
-				dos.flush();
-			} 
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
--- src/Server.java
+++ src/Server.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -20,12 +18,6 @@ public class Server {
 	public synchronized static void removeSocket(PrintWriter socket) {
 		list.remove(socket);
 	}
-	public synchronized static void output(String message) {
-		for(PrintWriter writer : list) {
-			writer.println(message);
-			writer.flush();
-		}
-	}
 	public static void main(String[] args) {
 		try (ServerSocket server = new ServerSocket(PORT)) {
 			while (true) {
@@ -33,9 +25,6 @@ public class Server {
 				Socket socket = server.accept();
 				
 				System.out.println("클라이언트 접속 중...");
-//				Socket client = server.accept();
-				ChatThread chatThread = new ChatThread(socket);
-				chatThread.start();
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -43,40 +32,4 @@ public class Server {
 			System.out.println("클라이언트 접속 종료...");
 		}
 	}
-}
-
-class ChatThread extends Thread {
-	private Socket socket;
-	private BufferedReader reader;
-	private PrintWriter writer;
-	
-	public ChatThread(Socket socket) {
-		super();
-		this.socket = socket;
-		try {
-			reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			writer = new PrintWriter(socket.getOutputStream());
-						Server.addSocket(writer);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	public void run() {
-		try {
-			String message = null;
-			while ((message = reader.readLine()) != null) {
-				Server.output(message);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				Server.removeSocket(writer);
-				socket.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	
 }
